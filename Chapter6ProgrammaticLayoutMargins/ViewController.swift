d12d3c4ea91cf2226edd1a63896c5d11bd3c5471
Herve Desrosiers <hdesrosiers@stingray.com>
2020-03-06 09:19:03 -0500

    Initial Commit

---
+++ Chapter6ProgrammaticLayoutMargins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter6ProgrammaticLayoutMargins
+//
+//  Created by Herve Desrosiers on 2020-03-06.
+//  Copyright © 2020 Herve Desrosiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
